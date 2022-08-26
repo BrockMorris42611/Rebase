4e25b0c387cbe07bfb6383d7be1eb7bf9c5c8a39
BrockMorris42611 <brock36morris@gmail.com>
2022-08-26 13:45:39 -0400

    new print

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("world");
         int x = 10;
         int z  = 9;
         int y = 11;
